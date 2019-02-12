c03596473f9ff3ed06ddf01edf2df34e962db681
YzakV <isaac.martinez@lakmisystems.com.mx>
2019-02-12 14:15:26 -0600

    Se modifico la linea de variables en app.js. Se anido el contenido en index.html

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ b.onclick = (e)=>{
    alert("Hola");
 };
 
-let h = "Hello World";
-let i = "Isaac Vega";
-let a = "Vega";
-let x = "Hola";
-let c = 1234;
+let h = "Hello World", i = "Isaac Vega", a = "Vega", x = "Hola", c = 1234;
 function ejecutar(){c("Hi");}
+
 
